418b05a42a08d977d3d32528693cac406ef07af4
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-08-09 12:46:33 +0800

    wew

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,29 @@ class DatabaseSeeder extends Seeder
             'updated_at' => now()
         ]);
 
+        \DB::table('users')->insert([
+            'email' => 'kradjumli@gmail.com',
+            'password' => bcrypt('dostregion9'),
+            'type' => 'Administrator',
+            'status' => 'Active',
+            'created_at' => now(),
+            'updated_at' => now(),
+            'email_verified_at' => now(),
+        ]);
+
+        \DB::table('user_profiles')->insert([
+            'firstname' => 'Ra-ouf',
+            'lastname' => 'Jumli',
+            'middlename' => 'Indanan',
+            'avatar' => 'avatar.jpg',
+            'mobile_no' => '00000000000',
+            'birthdate' => '2021-06-22',
+            'gender' => 'Male',
+            'user_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
         \DB::table('dropdowns')->insert([
             'id' => 1,
             'name' => 'n/a',
